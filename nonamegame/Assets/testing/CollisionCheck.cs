0442c68336b52941f2a7600802c439a44de7a48b
_tireDep <wing997@naver.com>
2019-03-18 22:12:11 +0900

    파일 생성 및 테스트 1. 버튼을 누를 시 큐브 생성 2. 키보드 입력시 이동 3. 충돌 구현(아직 완료x)

---
+++ nonamegame/Assets/testing/CollisionCheck.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionCheck : MonoBehaviour
+{
+    private void Update()
+    {
+        Move();
+    }
+
+    public int speed = 10;
+    void Move()
+    {
+        if(Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            transform.Translate(Vector3.right * speed * Time.deltaTime);
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            transform.Translate(Vector3.left * speed * Time.deltaTime);
+        }
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            transform.Translate(Vector3.up* speed *Time.deltaTime);
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            transform.Translate(Vector3.down * speed * Time.deltaTime);
+        }
+    }
+
+    Material material;
+    Color baseColor;
+    void OnCollisionEnter(Collision collision)
+    {
+        material.color = Color.yellow;
+    }
+
+    void OnCollisionExit(Collision collision)
+    {
+        material.color = baseColor;
+    }
+
+}
