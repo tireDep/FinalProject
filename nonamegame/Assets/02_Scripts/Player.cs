eab0420a78a123e26db8ac84acb8b5457f5f7452
_tireDep <wing997@naver.com>
2019-03-27 00:06:18 +0900

    프로토타입 제작01 - 임시 그래픽 적용 (32*32, 32*70(64로 수정예정)) - 캐릭터 이동 및 중력적용 - 임시 지형 생성

---
+++ nonamegame/Assets/02_Scripts/Player.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Rigidbody rigidbody;
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();      //Rigidbody 컴포넌트를 받아옴
+    }
+
+    void Update()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        transform.Translate(5f * Time.deltaTime, 0f, 0f);
+        // 플레이어 자동 이동
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //rigidbody.AddForce(Vector3.up * 5, ForceMode.Impulse);
+            transform.Translate(Vector3.up*2);
+            // 2단 점프도 생각해볼것
+        }
+    }
+}
---
+++ nonamegame/Assets/02_Scripts/Reset.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Reset : MonoBehaviour
+{
+    public Player player;
+
+    public void WhenClick()
+    {
+        player.transform.Translate(-10,1,0);
+    }
+}
