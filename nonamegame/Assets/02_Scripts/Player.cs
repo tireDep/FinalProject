79969e9b6a74a51acda0fdc09285758389f9e84f
_tireDep <wing997@naver.com>
2019-03-29 00:29:07 +0900

    프로토타입 제작 02 - 점프 수정 -> tag이용 무한점프 x -> 힘 +23
    - 중력 수정
    -> +-100 중력으로 변경
    
    - 위치반전&중력반전 추가
    -> 키 입력시 반전됨
    
    - 소스코드 수정 및 정리

--- nonamegame/Assets/02_Scripts/Player.cs
+++ nonamegame/Assets/02_Scripts/Player.cs
@@ -4,27 +4,94 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    private Rigidbody rigidbody;
+    private Rigidbody rigidBody;    // 물리변수
+    private bool playerPos; // 플레이어 위치
+    /* maybe : 양방향도 추가될 수 있음 -> int형 수정? */
+    private bool isGround;  // 바닥 체크
+
     void Start()
     {
-        rigidbody = GetComponent<Rigidbody>();      //Rigidbody 컴포넌트를 받아옴
-    }
+        rigidBody = GetComponent<Rigidbody>();      //Rigidbody 컴포넌트를 받아옴
+        playerPos = true;   // 위에 위치
+        isGround = true;    // 점프 가능
+    }   // Start()
 
     void Update()
     {
         Move();
-    }
+    }   // Update()
 
-    void Move()
+    private void Move() // 플레이어 이동 관련
     {
-        transform.Translate(5f * Time.deltaTime, 0f, 0f);
-        // 플레이어 자동 이동
+        transform.Translate(5f * Time.deltaTime, 0f, 0f);   // 플레이어 자동 이동
 
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            //rigidbody.AddForce(Vector3.up * 5, ForceMode.Impulse);
-            transform.Translate(Vector3.up*2);
-            // 2단 점프도 생각해볼것
+            PlayerJump();
         }
-    }
-}
+
+        if (Input.GetKeyDown(KeyCode.UpArrow)||Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            CheckPlayerPos();
+        }
+    }   // Move()
+
+    private void PlayerJump()   // 플레이어 점프
+    {
+        if(isGround)
+        {
+            //isGround = false;
+            if (playerPos)
+            {
+                rigidBody.AddForce(Vector3.up * 23, ForceMode.Impulse);
+            }
+            else
+            {
+                rigidBody.AddForce(Vector3.down * 23, ForceMode.Impulse);
+            }
+        }
+    }   // PlayerJump()
+
+    private void CheckPlayerPos()   // 플레이어 위치 판별
+    {
+        if (playerPos == true)  // 위에서 아래로
+        {
+            playerPos = false;
+            Physics.gravity = Vector3.up * 100;
+            transform.Translate(Vector3.down * 2);
+        }
+        else if(playerPos == false) // 아래에서 위로
+        {
+            playerPos = true;
+            Physics.gravity = Vector3.down * 100;
+            transform.Translate(Vector3.up * 2);
+        }
+    }   // CheckPlayerPos()
+
+    // 점프 횟수 제한 관련
+    /* maybe : 점프 빠르게 연타하면 에러나는듯? 확인 필요함! */
+    void OnCollisionEnter(Collision collision)  // 처음 부딪혔을 경우
+    {
+        if (collision.transform.tag == "Ground")
+        {
+            isGround = true; //바닥과 맞닿아 있음(점프 가능)
+        }
+    }   // OnCollisionEnter(Collision collision)
+
+    void OnCollisionStay(Collision collision)   // 부딪힌 상태 유지
+    {
+        if (collision.transform.tag == "Ground")
+        {
+            isGround = true; //바닥과 맞닿아 있음(점프 가능)
+        }
+    }   // OnCollisionStay(Collision collision)   
+
+    void OnCollisionExit(Collision collision)    // 콜라이더 떨어져 있을 때
+    {
+        if (collision.transform.tag == "Ground")
+        {
+            isGround = false; //바닥과 맞닿아 있지 않음(점프 불가능)
+        }
+    }   // OnCollisionExit(Collision collision)    
+
+}   // Class
