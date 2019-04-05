8ec0d107f1bd0fc992837764d70e1a5bb8423a9c
_tireDep <wing997@naver.com>
2019-04-05 20:33:53 +0900

    커밋내용 수정

---
+++ nonamegame/Assets/02_Scripts/Interface.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;  // Scene change
+
+public class Interface : MonoBehaviour
+{
+
+    public void GameStart() // 게임화면 이동
+    {
+        SceneManager.LoadScene("MainGame");
+    }
+
+    public void GameQuit()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+        // 유니티 에디터 종료
+#elif UNITY_WEBPLAYER
+        Application.OpenURL("http://google.com");
+        // 웹 종료 -> 구글화면
+#else
+        Application.Quit();
+        // 프로그램 종료
+#endif
+    }
+
+    public void Continue()
+    {
+        bool getPause = GameObject.Find("isPause").GetComponent<Player>();
+        Time.timeScale = 1;
+        getPause = true;
+
+    }
+ 
+}
