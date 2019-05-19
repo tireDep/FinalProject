28097323c8ffa4ceb7969f9082d30836605e2599
_tireDep <wing997@naver.com>
2019-05-20 01:29:27 +0900

    < Today List > - 스와이프 임시 적용

---
+++ nonamegame/Assets/02_Scripts/MobileSwipe.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MobileSwipe : SwipeReceiver
+{
+    protected override void OnSwipeUp()
+    {
+        base.OnSwipeUp();
+        Player.PlayerJump();
+    }
+
+    protected override void OnSwipeDonw()
+    {
+        base.OnSwipeDonw();
+        Player.PlayerJump();
+    }
+
+    /*protected override void OnSwipeLeft()
+    {
+        base.OnSwipeLeft();
+        Player.CheckPlayerPos();
+    }
+
+    protected override void OnSwipeRight()
+    {
+        base.OnSwipeRight();
+        Player.CheckPlayerPos();
+    }*/
+
+}
---
+++ nonamegame/Assets/02_Scripts/SwipeReceiver.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems; 
+
+public class SwipeReceiver : MonoBehaviour
+{
+    //override this
+    protected virtual void OnSwipeLeft()
+    {
+
+    }
+
+    //override this
+    protected virtual void OnSwipeRight()
+    {
+
+    }
+
+    //override this
+    protected virtual void OnSwipeUp()
+    {
+
+    }
+
+    //override this
+    protected virtual void OnSwipeDonw()
+    {
+
+    }
+
+    protected virtual void Update()
+    {
+        if (!Game.isPause && EventSystem.current.IsPointerOverGameObject() == false)
+        {
+            if (Input.touchCount > 0)
+            {
+                Touch touch = Input.GetTouch(0);
+                Player.CheckPlayerPos();
+            }
+            else
+            {
+                if (SwipeManager.Instance.IsSwiping(SwipeManager.SwipeDirection.Right))
+                {
+                    OnSwipeRight();
+                }
+
+                if (SwipeManager.Instance.IsSwiping(SwipeManager.SwipeDirection.Left))
+                {
+                    OnSwipeLeft();
+                }
+
+                if (SwipeManager.Instance.IsSwiping(SwipeManager.SwipeDirection.Up))
+                {
+                    OnSwipeUp();
+                }
+
+                if (SwipeManager.Instance.IsSwiping(SwipeManager.SwipeDirection.Down))
+                {
+                    OnSwipeDonw();
+                }
+            }
+        }
+    }
+
+}
